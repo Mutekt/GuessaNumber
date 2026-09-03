dd38ef9062e15457422ccbfd3c6d9fe197461eed
Mutekt <98408302+Mutekt@users.noreply.github.com>
2026-09-03 13:05:00 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This program generates a radnom number and the player has to guess it
+
+new update:
+-information if the number is too high or too low
+-showing attempts
--- maincode.cpp
+++ maincode.cpp
@@ -7,7 +7,9 @@ int main(){
     
     bool game=1;  //game is running
 
-    int randomNum = rand()% 100+1; // generates number from 0 to 100
+    int randomNum = rand()% 100+1; // generates number from 1 to 100
+
+    int attempts=0; // declares the attempts
 
     while (game==true){ // program is running until the game is set to something else then true
 
@@ -16,14 +18,28 @@ int main(){
         int number=0; //declares the later input
 
         std::cin>> number; //asks for number
+        if (std::cin.fail()){
+            std::cout<<"input must be a number!\n";
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+        }
 
-        if (number==randomNum){ //checks if the number is equal to the generated one
+        else if (number>randomNum){ 
+            std::cout<<"Too high!\n"; //informs that the input is too high
+            attempts++; // adds one to attempts
+        }
+        
+        else if (number<randomNum){ 
 
-            std::cout<<"You found the number!\n";
-
+            std::cout<<"Too low!\n"; //informs that the input is too low
+            attempts++; //adds one to attempts
+        }
+        else {
+            attempts++; // adds one to attempts
+            std::cout<<"You found the number, it took you " <<attempts<<" attempts\n"; //shows how many attempts it took
             game=0; //if its equal it turns game to false that ends the loop
+        }
 
-        };
         
     };
     return 0;
